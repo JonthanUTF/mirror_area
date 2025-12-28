44a872c4553f26614d90123eac9e10709c788bb7
Keyvan <keyvan.goddard@epitech.eu>
2025-12-28 22:53:37 +0100

    feat(front-web): add private route, reachable only if logged

--- front-web/src/App.jsx
+++ front-web/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, Outlet } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
@@ -7,15 +7,33 @@ import CreateActionReaction from "./pages/createActionReaction";
 import AuthCallback from "./pages/AuthCallback";
 import ServicesCallback from "./pages/servicesCallback.jsx";
 
+
+const PrivateRoute = () => {
+  let auth = localStorage.getItem("authToken")
+
+  if (!auth) {
+    return <Navigate to="/login" replace />;
+  }
+  return <Outlet />;
+}
+
 function App() {
+  console.log("TEST log");
+  localStorage.setItem("authToken", "");
   return (
     <Routes>
+      {/* Public routes */}
       <Route path="/login" element={<Login />} />
-      <Route path="/createActionReaction" element={<CreateActionReaction />} />
-      <Route path="/Signup" element={<Signup />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/auth/callback" element={<AuthCallback />} />
-      <Route path="/services/callback" element={<ServicesCallback />} />
+      <Route path="/signup" element={<Signup />} />
+      
+      {/* Protected routes */}
+      <Route element={<PrivateRoute/>}>
+        <Route path='/home' element={<Home/>} />
+        <Route path='/createActionReaction' element={<CreateActionReaction/>} />
+        <Route path='/services/callback' element={<ServicesCallback/>} />
+        <Route path="/auth/callback" element={<AuthCallback />} />
+      </Route>
+      
       <Route path="/" element={<Navigate to="/login" replace />} />
     </Routes>
   );
