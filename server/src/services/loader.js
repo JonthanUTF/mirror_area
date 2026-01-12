25f0a64f2fc2583889405eeec76e19ad3aef0795
Keyvan <keyvan.goddard@epitech.eu>
2026-01-12 18:25:21 +0100

    feat(front-web / backend): add microsoft service with OneDrive/Outlook actions/reactions

--- server/src/services/loader.js
+++ server/src/services/loader.js
@@ -6,6 +6,7 @@ const TimerService = require('./implementations/TimerService');
 const ConsoleService = require('./implementations/ConsoleService');
 const EmailService = require('./implementations/EmailService');
 const GitHubService = require('./implementations/GitHubService');
+const MicrosoftService = require('./implementations/MicrosoftService');
 
 function loadServices() {
     registry.register(WeatherService);
@@ -13,6 +14,7 @@ function loadServices() {
     registry.register(ConsoleService);
     registry.register(EmailService);
     registry.register(GitHubService);
+    registry.register(MicrosoftService);
 
     console.log('[LOADER] All services loaded into registry');
 }
