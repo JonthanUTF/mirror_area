7625092f4512a8b177b7e9eac886ad177b966da7
Keyvan <keyvan.goddard@epitech.eu>
2026-01-14 10:11:06 +0100

    feat(front-web): fix github/twitch service connection

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,9 +1,9 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'area_db',
-  process.env.DB_USER || 'area',
-  process.env.DB_PASSWORD || 'area',
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
